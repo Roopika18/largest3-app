cde34f12ec10128b4937ff8640e1986acd8e6641
ROOPIKA P <87657622+Roopika18@users.noreply.github.com>
2024-04-21 23:45:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ def find_largest(a, b, c):
     return max(a, b, c)
 
 st.title("Find the Largest Among Three Numbers : ")
-st.write("Enter three numbers below :")
+
 num1 = st.number_input("Enter 1st number", value=0)
 num2 = st.number_input("Enter 2nd number", value=0)
 num3 = st.number_input("Enter 3rd number", value=0)
